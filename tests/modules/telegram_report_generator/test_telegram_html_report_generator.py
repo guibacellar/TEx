7386e51770820d9eb1b7979d01d5654f754a5569
Guilherme Bacellar Moralez <guibacellar@gmail.com>
2023-08-17 08:37:30 -0300

    Added tests for telegram_html_report_generator
    Signed-off-by: Guilherme Bacellar Moralez <guibacellar@gmail.com>

--- tests/modules/telegram_report_generator/test_telegram_html_report_generator.py
+++ tests/modules/telegram_report_generator/test_telegram_html_report_generator.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import datetime
+import os.path
 import shutil
 import unittest
 from configparser import ConfigParser
@@ -15,7 +16,8 @@ from telethon.tl.functions.messages import GetDialogsRequest
 from TEx.core.dir_manager import DirectoryManagerUtils
 from TEx.database.db_initializer import DbInitializer
 from TEx.database.db_manager import DbManager
-from TEx.database.telegram_group_database import TelegramGroupDatabaseManager, TelegramMessageDatabaseManager
+from TEx.database.telegram_group_database import TelegramGroupDatabaseManager, TelegramMediaDatabaseManager, \
+    TelegramMessageDatabaseManager
 from TEx.models.database.telegram_db_model import (
     TelegramGroupOrmEntity,
     TelegramMediaOrmEntity, TelegramMessageOrmEntity, TelegramUserOrmEntity,
@@ -37,6 +39,7 @@ class TelegramGroupMessageScrapperTest(unittest.TestCase):
         DirectoryManagerUtils.ensure_dir_struct('_data')
         DirectoryManagerUtils.ensure_dir_struct('_data/resources')
         DirectoryManagerUtils.ensure_dir_struct('_data/media')
+        DirectoryManagerUtils.ensure_dir_struct('_data/media/2')
 
         DbInitializer.init(data_path='_data/')
 
@@ -46,13 +49,34 @@ class TelegramGroupMessageScrapperTest(unittest.TestCase):
         DbManager.SESSIONS['data'].execute(delete(TelegramMediaOrmEntity))
         DbManager.SESSIONS['data'].commit()
 
+        # Add Media
+        shutil.copy(
+            os.path.join('resources', 'mat.pdf'),
+            os.path.join('_data', 'media', '2', '55_mat.pdf')
+            )
+        media_id = TelegramMediaDatabaseManager.insert(
+            {
+                'file_name': '55_mat.pdf',
+                'telegram_id': 999,
+                'group_id': 2,
+                'extension': 'pdf',
+                'height': 0,
+                'width': 0,
+                'date_time': datetime.datetime.utcnow(),
+                'mime_type': 'application/pdf',
+                'size_bytes': 58964,
+                'title': 'mat pdf',
+                'name': 'mat'
+            }
+        )
+
         # Add Group 1 - Without Any Message
         TelegramGroupDatabaseManager.insert_or_update({
             'id': 1, 'constructor_id': 'A', 'access_hash': 'AAAAAA',
             'fake': False, 'gigagroup': False, 'has_geo': False,
             'participants_count': 1, 'restricted': False,
             'scam': False, 'group_username': 'UN-A',
-            'verified': False, 'title': 'UT-01', 'source': 'UT-PHONE'
+            'verified': False, 'title': 'UT-01', 'source': '5526986587745'
         })
 
         # Add Group 2 - With Previous Messages
@@ -61,12 +85,12 @@ class TelegramGroupMessageScrapperTest(unittest.TestCase):
             'fake': False, 'gigagroup': False, 'has_geo': False,
             'participants_count': 2, 'restricted': False,
             'scam': False, 'group_username': 'UN-b',
-            'verified': False, 'title': 'UT-02', 'source': 'UT-PHONE'
+            'verified': False, 'title': 'UT-02', 'source': '5526986587745'
         })
         TelegramMessageDatabaseManager.insert({
             'id': 55, 'group_id': 2, 'date_time': datetime.datetime.now(tz=pytz.utc),
             'message': 'Message 1', 'raw': 'Raw Message 1', 'from_id': None, 'from_type': None,
-            'to_id': None, 'media_id': None
+            'to_id': None, 'media_id': media_id
         })
         TelegramMessageDatabaseManager.insert({
             'id': 56, 'group_id': 2, 'date_time': datetime.datetime.now(tz=pytz.utc),
@@ -144,6 +168,38 @@ class TelegramGroupMessageScrapperTest(unittest.TestCase):
                     data=data
                 )
             )
+
+            # Check Log Messages
+            for m in captured.records:
+                print(m.message)
+
+            expected_log_messages = [
+                '\t\tFound 2 Groups',
+                '\t\tProcessing "UT-01" (1)',
+                '\t\t\tRetrieving Messages',
+                '\t\t\tFiltering',
+                '\t\tProcessing "UT-02" (2)',
+                '\t\t\tRetrieving Messages',
+                '\t\t\tFiltering',
+                '\t\t\tProcessing Messages',
+                '\t\t\tRendering',
+                '\t\t\tRendering Index Page'
+            ]
+
+            self.assertEqual(len(expected_log_messages), len(captured.records))
+            for ix, record in enumerate(captured.records):
+                self.assertEqual(expected_log_messages[ix], record.message)
+
+        # Check Output Files
+        self.assertTrue(
+            os.path.exists(os.path.join('_report', 'assets', '2_55_mat.pdf'))
+        )
+        self.assertTrue(
+            os.path.exists(os.path.join('_report', 'index.html'))
+        )
+        self.assertTrue(
+            os.path.exists(os.path.join('_report', 'result_UN-b_2.html'))
+        )
 
     def __load_execution_config(self, args, data):
 
