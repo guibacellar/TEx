3be38650746e5f3e5e163320d46540453af09f20
Guilherme Bacellar Moralez <guibacellar@gmail.com>
2023-11-03 14:29:37 -0300

    Changes in Documentation + Added New Test via .config file to reflect the Documentation examples

--- tests/finder/test_finder_engine.py
+++ tests/finder/test_finder_engine.py
@@ -64,9 +64,55 @@ class FinderEngineTest(unittest.TestCase):
             )
 
         # Check if Webhook was Executed
-        target.notification_engine.run.assert_awaited_once_with(
-            notifiers=['NOTIFIER.DISCORD.NOT_002'],
-            entity=message_entity,
-            rule_id='FINDER.RULE.UT_Finder_Demo',
-            source='+15558987453'
+        target.notification_engine.run.assert_has_awaits([
+            call(notifiers=['NOTIFIER.DISCORD.NOT_002'], entity=message_entity, rule_id='FINDER.RULE.UT_Finder_Demo', source='+15558987453'),
+            call(notifiers=['NOTIFIER.DISCORD.NOT_002'], entity=message_entity, rule_id='FINDER.RULE.UT_Finder_Demo_MultiLine', source='+15558987453'),
+            call(notifiers=['NOTIFIER.DISCORD.NOT_002'], entity=message_entity, rule_id='FINDER.RULE.UT_Finder_Demo_MultiLine_WithLineBreak', source='+15558987453'),
+        ])
+
+    def test_run_not_found(self):
+        """Test Run With Nothing Find."""
+
+        # Setup Mock
+        notifier_engine_mock = mock.AsyncMock()
+
+        message_entity: FinderNotificationMessageEntity = FinderNotificationMessageEntity(
+            date_time=datetime.datetime.utcnow(),
+            raw_text="Mocked term4 Raw Text",
+            group_name="Group 001",
+            group_id=123456,
+            from_id="1234",
+            to_id=9876,
+            reply_to_msg_id=5544,
+            message_id=969696,
+            is_reply=False,
+            downloaded_media_info=None,
         )
+
+        args: Dict = {
+            'config': 'unittest_configfile.config'
+        }
+        data: Dict = {}
+        TestsCommon.execute_basic_pipeline_steps_for_initialization(config=self.config, args=args, data=data)
+
+        with mock.patch('TEx.finder.finder_engine.NotifierEngine', return_value=notifier_engine_mock):
+            target: FinderEngine = FinderEngine()
+
+            # Execute Discord Notifier Configure Method
+            target.configure(
+                config=self.config
+            )
+            target.notification_engine = notifier_engine_mock
+
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(
+
+                # Invoke Test Target
+                target.run(
+                    entity=message_entity,
+                    source='+15558987453'
+                )
+            )
+
+        # Check if Webhook was Executed
+        target.notification_engine.run.assert_not_called()
